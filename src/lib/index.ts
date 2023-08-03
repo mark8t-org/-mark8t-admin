cba2883f921bddf54707cc82673e02a5202f69cd
Ryan Spice <spice.ryan@hotmail.com>
2023-08-03 14:18:07 -0400

    configure exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,14 @@
 // Reexport your entry components here
 
+import Admin from "./+Admin.svelte";
+import Modules from "./modules";
+
 import type Mark8tCore from "@mark8t/core";
 
 export type { Mark8tCore };
+
+export {
+	Modules
+}
+
+export default Admin;
